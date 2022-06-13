7273b382ad9bb8b0c03692c91f49bf774fc0d618
m0stafanasr <mostafanasr2020@gmail.com>
2022-06-13 21:57:54 +0200

    initializing application

---
+++ src/app/viewModels/orders.ts
@@ -0,0 +1,20 @@
+
+
+
+        export interface Product {
+            ProductId: number;
+            Quantity: number;
+        }
+    
+        export interface Order {
+            OrderId?: number;
+            OrderDate?: string;
+            UserId?: string;
+            Products?: Product[];
+            PaymentType?: string;
+        }
+    
+    
+    
+    
+
---
+++ src/app/viewModels/products.ts
@@ -0,0 +1,7 @@
+export interface Products {
+        ProductId: number;
+        ProductName: string;
+        ProductPrice: number;
+        AvailablePieces: number;
+        ProductImg: string;
+}
