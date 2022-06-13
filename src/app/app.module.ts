e6ccb11d30d7e9cb36397ae9e1958baa421d6671
m0stafanasr <mostafanasr2020@gmail.com>
2022-06-14 00:31:17 +0200

    products component added with lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { ProductsService } from './services/products.service';
 import { OrdersComponent } from './component/orders/orders.component';
+import { ProductsComponent } from './component/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    OrdersComponent
+    OrdersComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/products/products.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ProductsService } from 'src/app/services/products.service';
+import { Products } from 'src/app/viewModels/products';
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  allProducts = new BehaviorSubject<Products[]>([])
+  returnedProds = this.allProducts.asObservable();
+  products:Products[]=[]
+  constructor(private productService:ProductsService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(e=>this.products=e);
+   // this.returnedProds.subscribe(e=>this.products =e)
+   console.log('this.prod')
+  }
+  
+  add(id){
+    console.log(id+ 'added')
+  }
+
+
+}
